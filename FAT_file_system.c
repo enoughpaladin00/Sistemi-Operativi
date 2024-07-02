9aeb64e93579fd125b2a6bb54fcfa914bdaf8549
enoughpaladin00 <rossijacopo002@gmail.com>
2024-07-02 23:15:14 +0200

    DAY ONE

--- FAT_file_system.c
+++ FAT_file_system.c
@@ -1,5 +1,50 @@
 #include "FAT_file_system.h"
 
-int main(int argc, char** argv){
+/*
+Funzione che inizializza la mappatura del buffer e le variabili del file system
+*/
+void init_fs(const char *filename){
     
+    int fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    if(fd == -1) error_handle("open");
+
+    if(ftruncate(fd, BLOCK_SIZE * MAX_BLOCKS)) error_handle("ftruncate");
+
+    fs_buffer = mmap(NULL, BLOCK_SIZE * MAX_BLOCKS, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if(fs_buffer == MAP_FAILED) error_handle("mmap");
+
+    if(close(fd)) error_handle("close");
+
+    fs.current_dir = &fs.root;
+    fs.root.elemCount = 0;
+
+    for(int i = 0; i < MAX_BLOCKS; i++){
+        fs.fat[i] = 0;
+    }
+}
+
+/*
+
+*/
+void error_handle(const char* error){
+    dprintf(2, "ERRORE %s: %d\n",error, errno);
+    exit(-1);
+};
+
+int input_tokenize(char* input, char* cmd, char** args){
+    int count = 0;
+
+    char* temp = strcpy(temp, input);
+    strcpy(cmd, strtok(input, " "));
+    while(strtok(NULL, " ") || strtok(NULL, "\n")){
+        count++;
+    }
+
+    args = (char**) malloc(sizeof(char*)*count);
+    strtok(temp, " ");
+    for(int i = 0; i < count; i++){
+        args[i] = (char*)calloc(MAX_BUFFER_SIZE, sizeof(char));
+        strcpy(args[i], strtok(NULL, " "));
+    }
+    return count;
 }
--- FAT_file_system.h
+++ FAT_file_system.h
@@ -1,16 +1,62 @@
+#ifndef __FAT_H__
+#define __FAT_H__
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <string.h>
+
+void error_handle(const char* error);
+void init_fs(const char *filename);
+int input_tokenize(char* input, char* cmd, char** args);
 
 #define MAX_DIRNAME_SIZE 256
 #define MAX_DIR_SIZE 64
-
+#define MAX_BLOCKS 1024
+#define BLOCK_SIZE 512
+#define MAX_BUFFER_SIZE 1024
 
 typedef struct{
     char name[MAX_DIRNAME_SIZE];
+    int start;
+    int size;
     int is_dir;
+    //mode_t permissions;
 } DirectoryEntry;
 
 typedef struct{
     DirectoryEntry head[MAX_DIR_SIZE];
     int elemCount;
 } Directory;
+
+typedef struct{
+    int fat[MAX_BLOCKS];
+    Directory root;
+    Directory *current_dir;
+} FileSystem;
+
+static FileSystem fs;
+static char* fs_buffer;
+
+typedef struct{
+    char* data;
+    int start;
+    int size;
+} FileHandle;
+
+//Funzioni da implementare
+int createFile(char* fileName);
+int eraseFile(char* fileName);
+int writeOnFile(char* fileName, int byteNum, char* buffer);
+int readFromFile(char* fileName);
+int seek(char* fileName);
+int createDir(const char* dirName);
+int eraseDir(char* dirName);
+int changeDir(const char* path);
+int listDir(const char* dirPath);
+
+#endif
